56519588fb80306c4f7698b28f0dc76bf7e589f9
gitriyad <riyad.github@gmail.com>
2020-05-02 17:34:49 +0600

    Updates

---
+++ precache-manifest.6c3d70274d6656f19bad6d69a2eac3f0.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f477534ef72849784d85012851a6b496",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "9b125fe7859938acfefb",
+    "url": "/react/static/css/main.a651e266.chunk.css"
+  },
+  {
+    "revision": "8b42adb931a37d85133d",
+    "url": "/react/static/js/2.fd07c688.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.fd07c688.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b125fe7859938acfefb",
+    "url": "/react/static/js/main.e1a27343.chunk.js"
+  },
+  {
+    "revision": "708d1b443d5ae2f95da0",
+    "url": "/react/static/js/runtime-main.b9acccf8.js"
+  },
+  {
+    "revision": "5ef30838939ad50ad28209397ed9ef24",
+    "url": "/react/static/media/1.5ef30838.jpg"
+  },
+  {
+    "revision": "f613d203cd99b60bc07b2dabd2d0007d",
+    "url": "/react/static/media/10.f613d203.jpg"
+  },
+  {
+    "revision": "f0af0ea8569fa0a04b6df9484326d98c",
+    "url": "/react/static/media/11.f0af0ea8.jpg"
+  },
+  {
+    "revision": "c76c670e67d784510a8b363459fc2c6f",
+    "url": "/react/static/media/12.c76c670e.jpg"
+  },
+  {
+    "revision": "ff610210871b949f2d57f449ff7d049a",
+    "url": "/react/static/media/13.ff610210.jpg"
+  },
+  {
+    "revision": "ca0c4e6f5dd246a7deebf0a7fb93ac0f",
+    "url": "/react/static/media/14.ca0c4e6f.jpg"
+  },
+  {
+    "revision": "dc5485356018402137e1afdd3183db15",
+    "url": "/react/static/media/2.dc548535.jpg"
+  },
+  {
+    "revision": "8c1eb43ee57dad9a37e19e3e534248a8",
+    "url": "/react/static/media/3.8c1eb43e.jpg"
+  },
+  {
+    "revision": "00d1c13d08c45f34502749fa4f6505f2",
+    "url": "/react/static/media/4.00d1c13d.jpg"
+  },
+  {
+    "revision": "1dd11dcb4ba3b4fa2fef1ec1c27b51ce",
+    "url": "/react/static/media/5.1dd11dcb.jpg"
+  },
+  {
+    "revision": "0ae546fce2f90be95b15674a1e8681ba",
+    "url": "/react/static/media/6.0ae546fc.jpg"
+  },
+  {
+    "revision": "334621be85d5b22e9d8b2dae40760ce0",
+    "url": "/react/static/media/7.334621be.jpg"
+  },
+  {
+    "revision": "3ecd993dbc03b17ddb534113b0c3b112",
+    "url": "/react/static/media/8.3ecd993d.jpg"
+  },
+  {
+    "revision": "069cf3eaf4ca0134b54f3add75b6ee6b",
+    "url": "/react/static/media/9.069cf3ea.jpg"
+  }
+]);
